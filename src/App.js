bf9d1fbecbed9d27f0bcadcfd37e119cde28df9f
hyeonjoonpark <pjjoon1379@gmail.com>
2023-02-17 05:29:39 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer = {
+  'name' : '박현준',
+  'birthday' : '060327',
+  'gender' : '남자',
+  'job' : '부소마고'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Customer 
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
